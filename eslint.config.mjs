10e42dcc7c7e23aea66dc8b02a638956eb2c913c
当下的七炎 <2275025@qq.com>
2025-01-17 18:05:23 +0800

    重新部署

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals",),
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "off", // 或者 "warn" 如果仅希望警告
